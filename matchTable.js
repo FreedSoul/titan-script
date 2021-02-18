fcd09f306b686b8885b1f2b29f34faa12b50a449
FreedSoul <mattwork07@gmail.com>
2021-02-17 23:39:13 -0500

    Create matchTable.js

---
+++ matchTable.js
@@ -0,0 +1,10 @@
+export const matchTable = ['Shipper -- Consignee',
+  'Jacksonville Terminal -- Greenland RMC',
+  'Melbourne Terminal -- South Orange RMC',
+  'Melbourne Terminal -- Orlando Block',
+  'Melbourne Terminal -- Greenbay RMC',
+  'Jahna Greenbay -- South Orange RMC',
+  'Jahna Greenbay -- Daytona RMC',
+  'JAHNA GREENBAY -- Melbourne RMC',
+  'CORKSCREW MINE -- VENICE RMC',
+  'JAHNA GREENBAY -- ST CLOUD RMC']
